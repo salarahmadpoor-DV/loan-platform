0c97429438a00667d21e1805b1fb33076f818a8a
s.ahmadpour <s.ahmadpour@fwtech.com>
2026-08-05 10:21:47 +0330

    Add bank questions and loan request entities

--- Loan.Domain/Entities/Bank.cs
+++ Loan.Domain/Entities/Bank.cs
@@ -8,6 +8,32 @@ public class Bank : AuditableEntity
 
     public string Slug { get; private set; } = null!;
 
+    public string? ShortDescription { get; private set; }
+
+    public string? Description { get; private set; }
+
+    public string? TelegramLink { get; private set; }
+
+    public string? EitaaLink { get; private set; }
+
+    public string? RubikaLink { get; private set; }
+
+    public string? SeoTitle { get; private set; }
+
+    public string? SeoDescription { get; private set; }
+
+    public int SortOrder { get; private set; }
+
+    public bool IsActive { get; private set; } = true;
+
+
+    public ICollection<BankQuestion> Questions { get; private set; }
+        = new List<BankQuestion>();
+
+    public ICollection<LoanRequest> LoanRequests { get; private set; }
+        = new List<LoanRequest>();
+
+
     private Bank()
     {
     }
@@ -16,5 +42,6 @@ public class Bank : AuditableEntity
     {
         Title = title;
         Slug = slug;
+        IsActive = true;
     }
 }
